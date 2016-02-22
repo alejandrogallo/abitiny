ee14e9830d43a7ba7d036d473230dd898c266723
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-02-22 20:39:10 +0100

    basis

---
+++ abitiny/basis.py
@@ -0,0 +1,15 @@
+
+DEFAULT_BASIS_FILENAME = "basis.abi"
+
+class GaussianBasis(object):
+
+    """
+    Implementation of a gaussian basis
+    """
+
+    def __init__(self, filename=DEFAULT_BASIS_FILENAME):
+        self.fileName = filename
+
+    def parseInformation(self):
+        pass
+

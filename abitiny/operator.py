6dc36d685405cada33611ae40347a2a33fe59ed8
Alejandro Gallo <gallo@pccq05.fkf.mpg.de>
2016-02-23 20:09:19 +0100

    stuff

---
+++ abitiny/operator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import numpy
+
+class Operator(object):
+
+    def __init__(self):
+        """TODO: to be defined1. """
+    def getMatrix(self):
+        pass
